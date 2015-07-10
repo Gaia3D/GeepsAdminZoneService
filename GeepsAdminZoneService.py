aaece2c750ccc423f0f0bcf066033c92d9105846
jangbi <jangbi882@gmail.com>
2015-07-10 21:58:28 +0900

    한글 조회문제까지 해결

--- GeepsAdminZoneService.py
+++ GeepsAdminZoneService.py
@@ -66,6 +66,7 @@ def close_connection(exception):
 
 
 def query_db(query, args=(), one=False, cursor_factory=None):
+    logger.debug(query)
     try:
         if cursor_factory:
             cur = get_db().cursor(cursor_factory=cursor_factory)
@@ -81,28 +82,28 @@ def query_db(query, args=(), one=False, cursor_factory=None):
     return (rv[0] if rv else None) if one else rv
 
 
-### EVENT
-@app.route('/test')
-def hello():
-    return "GeepsAdminZoneService Activated!"
-
-
 def get_class1():
     return query_db("select distinct class1 from adminzone_meta order by class1")
 
 def get_class2(class1):
-    return query_db("select distinct class1, class2 from adminzone_meta where class1 = ? order by class1, class2", class1.encode("utf-8"))
+    return query_db("select distinct class1, class2 from adminzone_meta where class1 = '{}' order by class1, class2".format(class1.encode("utf-8")))
 
 def get_class3(class1, class2):
-    return query_db("select distinct class1, class2, class3 from adminzone_meta where class1 = ? and class2 = ? order by class1, class2, class3", class1, class2)
+    return query_db("select distinct class1, class2, class3 from adminzone_meta where class1 = ? and class2 = ? order by class1, class2, class3", class1.encode("utf-8"), class2.encode("utf-8"))
 
 def get_timing(class1, class2):
-    return query_db("select distinct class1, class2, timing from adminzone_meta where class1 = ? and class2 = ? order by class1, class2, timing", class1, class2)
+    return query_db("select distinct class1, class2, timing from adminzone_meta where class1 = '{}' and class2 = '{}' order by class1, class2, timing".format(class1.encode("utf-8"), class2.encode("utf-8")))
 
 def get_all_meta():
     # 결과를 col_name:value 딕셔너리로 만든다.
     # http://initd.org/psycopg/docs/extras.html
     return query_db("select * from adminzone_meta order by class1, class2, class3", cursor_factory=psycopg2.extras.NamedTupleCursor)
+
+
+### EVENT
+@app.route('/test')
+def hello():
+    return "GeepsAdminZoneService Activated!"
 
 
 @app.route('/api/get_class1')
@@ -113,21 +114,22 @@ def api_get_class1():
 
     ret = Response(json.dumps(out_list, ensure_ascii=False), mimetype='text/json')
     ret.content_encoding = 'utf-8'
-    ret.cache_control = "999999"
+    ret.headers.set("Cache-Control", "public, max-age=604800")
+
     return ret
 
 
 @app.route('/service_page')
 def service_page():
     class1_list = get_class1()
-#    class2_list = get_class2(unicode(class1_list[0][0]))
-#    timing_list = get_timing(class1_list[0][0], class2_list[0][0])
+    class2_list = get_class2(unicode(class1_list[0][0]))
+    timing_list = get_timing(class2_list[0][0], class2_list[0][1])
     all_meta = get_all_meta()
 
     return render_template("service_page.html",
                            class1_list=class1_list,
-#                           class2_list=class2_list,
-#                           timing_list=timing_list,
+                           class2_list=class2_list,
+                           timing_list=timing_list,
                            all_meta=all_meta)
 
 if __name__ == '__main__':
